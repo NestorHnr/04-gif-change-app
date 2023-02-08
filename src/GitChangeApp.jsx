ff497be4aa8aa200124e8156af242a07f5ad101e
Nestor Heredia <nestornhr27@hotmail.com>
2023-02-07 22:59:44 -0500

    agregando componentes

--- src/GitChangeApp.jsx
+++ src/GitChangeApp.jsx
@@ -1,11 +1,29 @@
-
-
-import React from "react";
+import { useState } from 'react';
 
 export const GitChangeApp = () => {
+    
+    const [categories, setCategories] = useState([ 'One Punch','Dragon ball']); 
+    
+    const onAddCategory = () =>{
+
+        setCategories(['valorant',...categories]);
+    }
+
     return (
         <>
+            {/* titulo */}
             <h1>GitChangeApp</h1>
+            
+            {/* input */}
+
+            {/* Listado de Gif */}
+            <button onClick={onAddCategory}>agregar</button>
+            <ol>
+                {categories.map( category => {
+                    return <li key = { category }>{ category }</li>
+                })}
+            </ol>
+                {/* Gif item */}
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { GitChangeApp } from './GitChangeApp';
 
+import './styles.css';
+
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
